176d5647550813c657f4406025f46cfd5723057c
NasreddinHodja <tbizetde@gmail.com>
2020-10-04 08:06:33 -0300

    collitions

--- main.py
+++ main.py
@@ -14,16 +14,35 @@ sprite = pygame.image.load('assets/chronomage.png')
 moving_right = False
 moving_left = False
 
-player_location = [50, 300]
+player_location = [50, 50]
+player_y_momentum = 0
+
+player_rect = pygame.Rect(player_location[0], player_location[1],
+                          sprite.get_width(), sprite.get_height())
+test_rect = pygame.Rect(100, 100, 100, 50)
 
 while running:
     screen.fill(0)
-    screen.blit(sprite, player_location)
+
+    if player_location[1] > WINDOW_SIZE[1] - sprite.get_height():
+        player_y_momentum *= -1
+    else:
+        player_y_momentum += 0.2
+
+    player_location[1] += player_y_momentum
 
     if moving_right:
         player_location[0] += 4
     if moving_left:
         player_location[0] -= 4
+
+    player_rect.x = player_location[0]
+    player_rect.y = player_location[1]
+
+    if player_rect.colliderect(test_rect):
+        pygame.draw.rect(screen, (255, 0, 0), test_rect)
+    else:
+        pygame.draw.rect(screen, (0, 0, 0), test_rect)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -41,6 +60,7 @@ while running:
             elif event.key == pygame.K_LEFT:
                 moving_left = False
 
+    screen.blit(sprite, player_location)
     pygame.display.update()
     clock.tick(60)
 
