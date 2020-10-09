33f5a795d95f8a6aa9554ea7fc3e2bc07982c167
NasreddinHodja <tbizetde@gmail.com>
2020-10-09 11:28:19 -0300

    draw_board function

--- tetris/main.py
+++ tetris/main.py
@@ -5,23 +5,74 @@ import pygame
 from tetromino import Tetromino
 
 WINDOW_SIZE = (600, 600)
+BOARD_POS = (0, 0)
+
+COLOR  = [
+    (0, 0, 0),
+    (44, 135, 245),
+    (66, 245, 78),
+    (245, 242, 66),
+    (245, 66, 66),
+    (66, 245, 233),
+    (242, 66, 245),
+    (255, 255, 255)
+]
+
+board = [[7, 7, 7, 7, 7, 7, 7, 7, 7, 7 , 7, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 7],
+         [7, 7, 7, 7, 7, 7, 7, 7, 7, 7 , 7, 7]]
+
+def draw_board(display):
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            rect = (j, i, 1, 1)
+
+            for k in range(len(COLOR)):
+                if(board[i][j] == k):
+                    pygame.draw.rect(display, COLOR[k], rect)
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
-    display = pygame.Surface((int(WINDOW_SIZE[0] / 10),
-                              int(WINDOW_SIZE[1] / 10)))
+    display = pygame.Surface((int(WINDOW_SIZE[0] / 20),
+                              int(WINDOW_SIZE[1] / 20)))
 
     running = True
     while running:
         display.fill(0)
 
+        draw_board(display)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.blit(pygame.transform.scale(display, WINDOW_SIZE), (0, 0))
+        screen.blit(pygame.transform.scale(display, WINDOW_SIZE), (25, 25))
         pygame.display.update()
         clock.tick(60)
 
